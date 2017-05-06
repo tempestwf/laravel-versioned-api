4c2f488ddf4d5de885c4a381bfac2e40dffd5130
Will Ferrer <wferrer@xfire.com>
2017-05-05 19:24:10 -0700

    Rearranging, adding an optional entity trait, and cleaning things up

--- app/API/V3/Repositories/UserRepository.php
+++ app/API/V3/Repositories/UserRepository.php
@@ -2,11 +2,13 @@
 
 namespace App\API\V3\Repositories;
 
+use App\API\V3\Entities\User;
 use App\Repositories\Repository;
-use TempestTools\AclMiddleware\Permissions\HasPermissionsQueryTrait;
+use TempestTools\AclMiddleware\Contracts\RepoHasPermissions;
+use TempestTools\AclMiddleware\Repository\HasPermissionsQueryTrait;
 
-class UserRepository extends Repository
+class UserRepository extends Repository implements RepoHasPermissions
 {
     use HasPermissionsQueryTrait;
-	protected $entity = \App\API\V3\Entities\User::class;
+	protected $entity = User::class;
 }
