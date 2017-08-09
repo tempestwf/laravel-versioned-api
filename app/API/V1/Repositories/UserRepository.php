722d0fc5843d8fe51d361d5541ec4d4e1f2f202b
Will Ferrer <wferrer@xfire.com>
2017-08-08 18:45:03 -0700

    Adding user read config

--- app/API/V1/Repositories/UserRepository.php
+++ app/API/V1/Repositories/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\API\V1\Repositories;
 
 use App\API\V1\Entities\User;
 use App\Repositories\Repository;
+use Doctrine\ORM\Query\Expr;
 use TempestTools\AclMiddleware\Contracts\RepoHasPermissions;
 use TempestTools\AclMiddleware\Repository\HasPermissionsQueryTrait;
 
@@ -20,8 +21,51 @@ class UserRepository extends Repository implements RepoHasPermissions
      */
     public function getTTConfig(): array
     {
+        $expr = new Expr();
+        /** @noinspection NullPointerExceptionInspection */
         return [
-            'default'=>[],
+            'default'=>[
+                'read'=>[
+                    'select'=>[
+                        'standardSelect'=>'partial u.{id, name, address, job}'
+                    ],
+                    'where'=>[
+                        'onlyCurrentUser'=>[
+                            'type'=>'and',
+                            'value'=>$expr->eq('id', $this->getArrayHelper()->parseArrayPath(['userEntity', 'id']))
+                        ]
+                    ]
+                ],
+                'permissions'=>[
+                    'where'=>[
+                        'fields'=>[
+                            'password'=>[
+                                'permissive'=>false
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            'user'=>[
+                'extends'=>':default'
+            ],
+            'admin'=>[
+                'extends'=>':default',
+                'read'=>[
+                    'where'=>[
+                        'onlyCurrentUser'=>null
+                    ]
+                ],
+                'permissions'=>[
+                    'where'=>[
+                        'fields'=>[
+                            'password'=>[
+                                'permissive'=>true
+                            ]
+                        ]
+                    ]
+                ]
+            ],
             'testing'=>[]
         ];
     }
