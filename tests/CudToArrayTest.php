4a3c2a076466676d3e1e4c85e28504365a747137
Will Ferrer <wferrer@xfire.com>
2017-09-15 16:23:33 -0700

    Shifting around some things on toArray functionality

--- tests/CudToArrayTest.php
+++ tests/CudToArrayTest.php
@@ -4,6 +4,7 @@ use App\API\V1\Entities\Artist;
 use App\API\V1\Entities\User;
 use App\API\V1\Repositories\ArtistRepository;
 use App\API\V1\Repositories\UserRepository;
+use TempestTools\Common\Doctrine\Transformers\ToArrayTransformer;
 use TempestTools\Crud\PHPUnit\CrudTestBaseAbstract;
 
 
@@ -12,7 +13,7 @@ class CudToArrayTest extends CrudTestBaseAbstract
 
 
     /**
-     * @group CudPrePopulate
+     * @group CudToArray
      * @throws Exception
      */
     public function testToArray () {
