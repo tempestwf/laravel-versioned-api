79bf05741c3a2b16782a45ba820f98795b719bb4
Will Ferrer <wferrer@xfire.com>
2017-10-05 18:32:50 -0700

    All core functionality verified

--- app/API/V1/Controllers/ArtistController.php
+++ app/API/V1/Controllers/ArtistController.php
@@ -46,7 +46,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onInit (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['arrayHelper']->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['onInit'] = $event->getEventArgs()->getArrayCopy();
     }
 
     /**
@@ -54,7 +61,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onPreIndex (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['controller']->getArrayHelper()->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['onPreIndex'] = $event->getEventArgs()->getArrayCopy();
     }
 
 
@@ -63,7 +77,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onPostIndex (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['controller']->getArrayHelper()->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['onPostIndex'] = $event->getEventArgs()->getArrayCopy();
     }
 
     /**
@@ -71,7 +92,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onPreStore (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['controller']->getArrayHelper()->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['onPreStore'] = $event->getEventArgs()->getArrayCopy();
     }
 
     /**
@@ -79,7 +107,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onPostStore (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['controller']->getArrayHelper()->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['onPostStore'] = $event->getEventArgs()->getArrayCopy();
     }
 
     /**
@@ -87,7 +122,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onPreShow (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['controller']->getArrayHelper()->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['onPreShow'] = $event->getEventArgs()->getArrayCopy();
     }
 
     /**
@@ -95,7 +137,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onPostShow (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['controller']->getArrayHelper()->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['onPostShow'] = $event->getEventArgs()->getArrayCopy();
     }
 
     /**
@@ -103,7 +152,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onPreUpdate (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['controller']->getArrayHelper()->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['onPreUpdate'] = $event->getEventArgs()->getArrayCopy();
     }
 
     /**
@@ -111,7 +167,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onPostUpdate (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['controller']->getArrayHelper()->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['arrayHelper'] = $event->getEventArgs()->getArrayCopy();
     }
 
     /**
@@ -119,7 +182,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onPreDestroy (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['controller']->getArrayHelper()->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['onPreDestroy'] = $event->getEventArgs()->getArrayCopy();
     }
 
     /**
@@ -127,7 +197,14 @@ class ArtistController extends APIControllerAbstract
      */
     public function onPostDestroy (SimpleEventContract $event):void
     {
-        //
+        $eventArgs = $event->getEventArgs();
+
+        /** @noinspection NullPointerExceptionInspection */
+        $array = $eventArgs['controller']->getArrayHelper()->getArray();
+        if (!isset($array['controllerEvents'])) {
+            $array['controllerEvents'] = [];
+        }
+        $array['controllerEvents']['onPostDestroy'] = $event->getEventArgs()->getArrayCopy();
     }
 
 }
