f8a997426e39c24e2f47446e47bd794c0c5f08c6
Jerome Erazo <monx.erazo@me.com>
2018-09-12 21:40:24 +0800

    - Adding health check endpoint - Versioning on some major tables - EmailVerificationNotification template update - LoginLockNotification template update - MultipleDomains support - Translations update (adding pt) - Route update - .env.example cleanup

---
+++ resources/lang/pt/email.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Email Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during email sending for various
+    | messages that we need to display to the recipient. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'subject_account_activation' => 'Ativação de Conta',
+    'activation_greetings' => 'Bem vinda :username,',
+    'lorem_ipsum' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+
+    'greetings' => 'Olá :username,',
+    'your_verification_code' => 'Seu código de verificação',
+    'verification_instruction' => 'Instrução de Verificação',
+    'activate_now' => 'Ative agora!',
+    'terms' => 'Termos',
+    'privacy' => 'Privacidade',
+    'unsubscribe' => 'Cancelar subscrição',
+
+    'auth_partial_lock_subject' => 'ATENÇÃO: Notificação de tentativa de login',
+    'auth_partial_lock_line_1' => 'Detectar atividade suspeita na sua conta.',
+    'auth_partial_lock_line_2' => 'Se você recebeu um e-mail de "login suspeito", significa que recentemente bloqueamos uma tentativa de acessar sua conta porque não tínhamos certeza de que realmente era você. Para ajudar a proteger sua conta, enviamos um e-mail para você quando notamos uma atividade de login incomum, como uma tentativa de fazer login em um local ou dispositivo diferente do normal.',
+
+    'auth_full_lock_subject' => 'ALERTA: Notificação de bloqueio de conta',
+    'auth_full_lock_line_1' => 'Detectar atividade suspeita na sua conta.',
+    'auth_full_lock_line_2' => 'Nós temporariamente bloqueamos sua conta, pois detectamos tentativas de login suspeitas. Entre em contato para reativar sua conta.',
+    'auth_full_lock_action' => 'Reativar conta',
+];
